685f294a7a58d34a81cf8ff146aa4108889ed118
Marko Krstic <mailme@markokrstic.com>
2023-01-09 11:39:21 +0100

    emmet clean setup

---
+++ admin.js
@@ -0,0 +1,32 @@
+require.config({ paths: { vs: 'node_modules/monaco-editor/min/vs' } });
+require(['vs/editor/editor.main'], function () {
+	php_script = monaco.editor.create(document.getElementById('php_script'), {
+		value: "",
+		language: 'php',
+		lineNumbers: true,
+		roundedSelection: false,
+		scrollBeyondLastLine: false,
+		readOnly: false,
+		glyphMargin: false,
+		vertical: 'auto',
+		horizontal: 'auto',
+		verticalScrollbarSize: 10,
+		horizontalScrollbarSize: 10,
+		theme: 'vs-dark',
+		wordWrap: 'wordWrapColumn',
+		minimap: { enabled: false },
+		wordWrapColumn: 120,
+		wordWrapMinified: true,
+		wrappingIndent: "indent",
+		automaticLayout: true,
+		lineHeight: 12 * 1.45,
+		fontSize: 12,
+		"autoIndent": true,
+		"formatOnPaste": true,
+		"formatOnType": true,
+		'bracketPairColorization.enabled': true,
+	});
+	jQuery( window ).resize(function() {
+		php_script.layout({});
+	});
+});
